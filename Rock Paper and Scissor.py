c08a8454587250c47c6324252819fca1bac856b1
omdotgif <bankimchandra.25bai10742@vitbhopal.ac.in>
2025-11-24 23:43:19 +0530

    Update Rock Paper and Scissor.py

--- Rock Paper and Scissor.py
+++ Rock Paper and Scissor.py
@@ -1,26 +1,21 @@
 import random
 
-# List of possible choices
 options = ["Rock", "Paper", "Scissors"]
 
-# Get user's input
-user_choice = input("Enter Rock, Paper, or Scissors: ")
+user = input("Enter Rock, Paper, or Scissors: ")
 
-# Computer's random choice
-computer_choice = random.choice(options)
+computer = random.choice(options)
 
-# Print choices
-print(f"\nYou chose: {user_choice}")
-print(f"Computer chose: {computer_choice}\n")
+print(f"\nYou chose: {user}")
+print(f"Computer chose: {computer}\n")
 
-# Determine the winner
-if user_choice == computer_choice:
+if user == computer:
     print("It's a tie!")
-elif user_choice == "Rock" and computer_choice == "Scissors":
+elif user == "Rock" and computer == "Scissors":
     print("You win!")
-elif user_choice == "Paper" and computer_choice == "Rock":
+elif user == "Paper" and computer == "Rock":
     print("You win!")
-elif user_choice == "Scissors" and computer_choice == "Paper":
+elif user == "Scissors" and computer == "Paper":
     print("You win!")
 else:
     print("Computer wins!")
